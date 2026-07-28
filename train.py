1022d982d9226f2cb209204c92f03269aa54954b
Yashwardhan Jangid <yashwardhanjangid29@gmail.com>
2026-07-28 21:42:38 +0530

    Update train number and API endpoint in train.py

--- train.py
+++ train.py
@@ -1,22 +1,19 @@
 import os
 import requests
 
-# GitHub Secrets
 RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-# Train Number
-TRAIN_NUMBER = "12345"   # <-- Isko baad me apni train number se replace karna
+TRAIN_NUMBER = "12956"   # <-- Apni train number yahan likh
 
-url = f"https://real-time-pnr-status-api-for-indian-railways.p.rapidapi.com/train/{TRAIN_NUMBER}"
+url = f"https://irctc-api5.p.rapidapi.com/live-status/{TRAIN_NUMBER}"
 
 headers = {
+    "Content-Type": "application/json",
     "x-rapidapi-key": RAPIDAPI_KEY,
-    "x-rapidapi-host": "real-time-pnr-status-api-for-indian-railways.p.rapidapi.com"
+    "x-rapidapi-host": "irctc-api5.p.rapidapi.com"
 }
 
 response = requests.get(url, headers=headers)
 
-print(response.status_code)
+print("Status Code:", response.status_code)
 print(response.text)
